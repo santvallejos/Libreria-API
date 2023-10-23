d7dd3f13b294a4878b200f4123fe4f99926a42e1
santvallejos <vallejossantiago1412@gmail.com>
2023-10-23 20:27:31 -0300

    Eliminar un libro por si ISBN

--- src/controller.js
+++ src/controller.js
@@ -22,6 +22,12 @@ class libroController{
         res.json({"Registros eliminados": result.affectedRows});
     }
 
+    async delete(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM libros WHERE isbn=(?)`, [libro.isbn]);
+        res.json({"Registros eliminados": result.affectedRows});
+    }
+
     async update(req, res){
         const libro = req.body;
         const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), añopublicacion=(?), isbn=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.añopublicacion, libro.isbn, libro.id]);
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ export const router = Router();
 
 router.get('/libros', libro.getAll);//Ruta en donde se encuentran los libros
 router.post('/libro', libro.add);//Ruta para agregar un libro
-router.delete('/libro', libro.delete);//Ruta para eliminar un libro
+router.delete('/libro', libro.delete);//Ruta para eliminar un libro por id
+router.delete('/libro', libro.delete);//Ruta para eliminar un libro por ISBN
 router.put('/libro', libro.update);//Ruta para actualizar un libro
 router.get('/libro', libro.getOne);//Consultar id
